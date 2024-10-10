ba98331dda4c539340e687a4ee3ad8e5c9d41ee9
HojinHa <skdhksj@gmail.com>
2024-10-10 13:44:46 +0900

    eddd
    dd

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@
 
 int main(int argc, const char** argv) // main 함수, 가장 처음 실행 되는 함수
 {
-    std::cout << "Hello World!\n"; // std::cout을 이용하여 "Hello World"를 출력
+    std::cout << "Hello World!222\n"; // std::cout을 이용하여 "Hello World"를 출력
     return 0;
 }

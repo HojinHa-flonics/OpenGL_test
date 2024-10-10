2761746384dd7f52727093ae3823a663a6fd1981
HojinHa <skdhksj@gmail.com>
2024-10-10 13:43:39 +0900

    첫번째 커밋 테스트

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Cpp1_3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main(int argc, const char** argv) // main 함수, 가장 처음 실행 되는 함수
+{
+    std::cout << "Hello World!\n"; // std::cout을 이용하여 "Hello World"를 출력
+    return 0;
+}
